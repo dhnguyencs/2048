b3c765ea439a812d37a1954a7e53310a82482510
blasphemly <97411482+blasphemly@users.noreply.github.com>
2022-02-21 23:18:22 -0800

    Update 2048helper.js

--- 2048helper.js
+++ 2048helper.js
@@ -35,8 +35,10 @@ function initializeStuff(){
     for(i = 0; i < optionSelectionElements.length; i++){
         optionSelectionElements[i].addEventListener('click', function(){
             createBoard(this.id);
-            document.getElementById("title").style.fontSize = 100 + "px";
-            if (this.id == 5) {
+            if (this.id == 4) {
+                document.getElementById("title").style.fontSize = 100 + "px";
+                document.getElementById("title").innerText = "2048";
+            }if (this.id == 5) {
                 document.getElementById("title").innerText = "4096";
             } else if (this.id == 6) {
                 document.getElementById("title").innerText = "8192";
